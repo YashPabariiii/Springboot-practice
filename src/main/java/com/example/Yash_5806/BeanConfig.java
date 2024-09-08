15bcbc56eb6341f65f436d3e0b6f462d0d3b1d9b
Yash Pabari <yashpabari49@gmail.com>
2024-09-09 00:14:45 +0530

    Update BeanConfig.java

--- src/main/java/com/example/Yash_5806/BeanConfig.java
+++ src/main/java/com/example/Yash_5806/BeanConfig.java
@@ -1,4 +1,9 @@
 package com.example.Yash_5806;
 
+@Configuration
+@ComponentScan(basePackages = "com.example.Yash_5806")  
 public class BeanConfig {
+  @Bean
+  public Doctor doctor(){
+    return new Doctor();
 }
